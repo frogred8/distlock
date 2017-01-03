f48dcbd76bdbebe55129819aa888d9df04959fdc
frogred8 <frogred8@gmail.com>
2017-01-03 14:40:19 +0900

    delete typings

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/bluebird/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="modules/node-uuid/index.d.ts" />
-/// <reference path="modules/redis/index.d.ts" />
